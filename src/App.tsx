1b382bff6387455d8439c7072ec4751625a61386
jahangeer <jkniazi19@gmail.com>
2022-05-30 09:22:56 +0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
 import React, { useState } from 'react';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Navbar from './Components/Dashboard/Navbar';
-import Profile from './Components/Dashboard/Profile/index';
-import DetailPage from './Components/Dashboard/Todo/DetailPage';
-import Todo from './Components/Dashboard/Todo';
-import { Cart } from './store/cartStore';
-import Weather from './Components/Dashboard/Weather';
+import Navbar from './Parent/Components/Dashboard/Navbar';
+
 
 
 
